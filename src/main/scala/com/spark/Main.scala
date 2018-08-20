0c4cd75a4bc0c2cdf751754053a1163f9eb1d7b4
kailin2017 <kailin2017@users.noreply.github.com>
2018-08-20 20:14:12 +0800

    018.8.20

--- src/main/scala/com/spark/Main.scala
+++ src/main/scala/com/spark/Main.scala
@@ -7,7 +7,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF);
     System.setProperty("spark.ui.showConsoleProgress", "false");
-    val inputFile = "C:/Users/Kailin/Downloads/word.txt"
+    val inputFile = "./word.txt"
     val conf = new SparkConf().setAppName("WordCount").setMaster("local")
     val sc = new SparkContext(conf)
     val textFile = sc.textFile(inputFile)
